03dd86da5f83b8259db29dfbf1493c7c90085d98
Euan Chua <crxrrllubxjclybrlrzsyyvpmxasvlmlwngyvzzr@eu4nza.me>
2025-03-17 18:11:48 +0800

    updated media design

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,10 @@
 // footer.tsx
-import { FaFacebook } from "react-icons/fa6";
 import Image from "next/image";
 import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="max-w-screen py-2 flex justify-between items-center bg-white shadow-md md:shadow-lg px-6 md:px-32 relative z-10">
+    <footer className="max-w-screen py-2 flex justify-between items-center shadow-md md:shadow-lg px-6 md:px-32 relative z-10">
       <Link href="/" className="flex flex-row items-center gap-2">
         <Image
           src="/assets/logo/minhs_logo.webp"
@@ -19,15 +18,20 @@ export function Footer() {
           <p>National High School</p>
         </div>
       </Link>
-
-      <a
-        href="https://web.facebook.com/profile.php?id=100065310557489"
-        target="_blank"
-        className="flex flex-row items-center gap-2"
-      >
-        <FaFacebook />
-        <span>Facebook</span>
-      </a>
+      <span className="flex flex-row items-center gap-2">
+        Visit us on:
+        <a
+          href="https://web.facebook.com/profile.php?id=100065310557489"
+          target="_blank"
+        >
+          <Image
+            src="/assets/media/Facebook_Logo_Primary.png"
+            alt="Facebook Logo"
+            width={20}
+            height={20}
+          />
+        </a>
+      </span>
     </footer>
   );
 }
